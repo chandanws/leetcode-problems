5f2e247f4cc82d3c06d055893c53d72516a5f208
PoAn (Baron) Chen <chen.baron@hotmail.com>
2017-10-12 19:36:07 -0700

    some clean up of code
    * Removed the shouldBreak, result variable

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -6,19 +6,13 @@ import java.util.*;
 
 class Solution {
   public int[] twoSum(int[] nums, int target) {
-    boolean shouldBreak = false;
-    int [] result = null;
-
-    for (int i = 0; i < nums.length && !shouldBreak; i++) {
+    for (int i = 0; i < nums.length; i++) {
       for (int j = i + 1; j < nums.length; j++) {
         if (nums[i] + nums[j] == target) {
-          result = new int[]{i, j};
-          shouldBreak = true;
-          break;
+          return new int [] {i, j};
         }
       }
     }
-
-    return result;
+    throw new IllegalArgumentException("No two sum solution");
   }
 }
