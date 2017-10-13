02a8e0e8546375e7be5c6c46aba55f6d3c25d0bd
PoAn (Baron) Chen <chen.baron@hotmail.com>
2017-10-12 19:50:42 -0700

    Improved the complexity by using HashMap
    The complexity should now be O(n) instead of O(n**2)

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -6,12 +6,13 @@ import java.util.*;
 
 class Solution {
   public int[] twoSum(int[] nums, int target) {
+    HashMap <Integer, Integer> map = new HashMap <>();
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return new int [] {i, j};
-        }
+      int diff = target - nums[i];
+      if (map.containsKey(diff) && map.get(diff) != i) {
+        return new int [] {map.get(diff), i};
       }
+      map.put(nums[i], i);
     }
     throw new IllegalArgumentException("No two sum solution");
   }
