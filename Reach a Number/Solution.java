eafcff81664f98fd8ea117dac90b6c8911a184c6
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-02-04 20:47:41 -0800

    Update Solution.java
    added solution that got accpeted

--- Reach a Number/Solution.java
+++ Reach a Number/Solution.java
@@ -1,26 +1,37 @@
 // Problem link: https://leetcode.com/problems/copy-list-with-random-pointer/description/
-// Status: Time Limit Exceeded
+// Status: Accepted
 
 class Solution {
     public int reachNumber(int target) {
-        int numberOfSteps = 0, currentPos = Math.abs(target);
-        if(target != 0) {
-          numberOfSteps = (int)Math.floor(Math.sqrt(target*2)) - 1;
-        }
-        while(currentPos != 0) {
-          numberOfSteps++;
-          currentPos = Math.abs(target);
-          int tryingNumberOfSteps = numberOfSteps;
-          while(numberOfSteps > 0) {
-            if(Math.abs(currentPos - numberOfSteps) < Math.abs(currentPos + numberOfSteps)) {
-              currentPos = currentPos - numberOfSteps;
-            } else {
-              currentPos = currentPos + numberOfSteps;
-            }
-            numberOfSteps--;
-          }
-          numberOfSteps = tryingNumberOfSteps;
+        int numberOfSteps = 0, sum = 0;
+        while(sum < Math.abs(target) || (sum - Math.abs(target)) % 2 != 0) {
+          sum += ++numberOfSteps;
         }
         return numberOfSteps;
     }
 }
+
+// Status: Time Limit Exceeded
+// class Solution {
+//     public int reachNumber(int target) {
+//         int numberOfSteps = 0, currentPos = Math.abs(target);
+//         if(target != 0) {
+//           numberOfSteps = (int)Math.floor(Math.sqrt(target*2)) - 1;
+//         }
+//         while(currentPos != 0) {
+//           numberOfSteps++;
+//           currentPos = Math.abs(target);
+//           int tryingNumberOfSteps = numberOfSteps;
+//           while(numberOfSteps > 0) {
+//             if(Math.abs(currentPos - numberOfSteps) < Math.abs(currentPos + numberOfSteps)) {
+//               currentPos = currentPos - numberOfSteps;
+//             } else {
+//               currentPos = currentPos + numberOfSteps;
+//             }
+//             numberOfSteps--;
+//           }
+//           numberOfSteps = tryingNumberOfSteps;
+//         }
+//         return numberOfSteps;
+//     }
+// }
