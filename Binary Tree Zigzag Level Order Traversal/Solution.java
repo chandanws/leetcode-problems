004a5e43e4fd5773228f85703e9c95b892a32685
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-03-29 23:02:15 -0700

    Update Solution.java
    added way using arraylist as stack (normally you would be able to use int array as fake stack however, this case we have to because we do not know the size of the int array.

--- Binary Tree Zigzag Level Order Traversal/Solution.java
+++ Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -11,7 +11,7 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrderUsingStack(TreeNode root) {
         List<List<Integer>> l = new ArrayList<List<Integer>>();
         if(root == null) return l;
         Stack<TreeNode> s1 = new Stack<>();
@@ -32,6 +32,40 @@ class Solution {
                 if(current.right != null) pushS.push(current.right);
             }
             if(popS.isEmpty()) level++;
+        }
+        return l;
+    }
+    public List<List<Integer>> zigzagLevelOrderUsingArrayListAsStack(TreeNode root) {
+        List<List<Integer>> l = new ArrayList<List<Integer>>();
+        if(root == null) return l;
+        ArrayList<TreeNode> s1 = new ArrayList<>(Arrays.asList(root));
+        ArrayList<TreeNode> s2 = new ArrayList<>();
+        int s1Top = 0, s2Top = -1;
+        int level = 0;
+        while(s1Top != -1 || s2Top != -1) {
+            ArrayList<TreeNode> popS = level % 2 == 0 ? s1 : s2;
+            ArrayList<TreeNode> pushS = level % 2 == 0 ? s2 : s1;
+            int popSTop = level % 2 == 0 ? s1Top : s2Top;
+            int pushSTop = level % 2 == 0 ? s2Top : s1Top;
+            TreeNode current = popS.get(popSTop);
+            popS.remove(popSTop--);
+            if(l.size() < level + 1) l.add(new ArrayList<Integer>(Arrays.asList(current.val)));
+            else l.get(level).add(current.val);
+            if((level + 1) % 2 == 0) {
+                if(current.right != null) pushS.add(++pushSTop, current.right);
+                if(current.left != null) pushS.add(++pushSTop, current.left);
+            } else {
+                if(current.left != null) pushS.add(++pushSTop, current.left);
+                if(current.right != null) pushS.add(++pushSTop, current.right);
+            }
+            if(level % 2 == 0) {
+                s1Top = popSTop;
+                s2Top = pushSTop;
+            } else {
+                s1Top = pushSTop;
+                s2Top = popSTop;
+            }
+            if(popSTop == -1) level++;
         }
         return l;
     }
