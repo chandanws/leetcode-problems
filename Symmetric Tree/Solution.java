fe9447fe61e7b32e3fbbca94faf6772c6fe8692e
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-03-30 21:25:19 -0700

    Update Solution.java
    added iteratively approach

--- Symmetric Tree/Solution.java
+++ Symmetric Tree/Solution.java
@@ -11,7 +11,7 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetricRecur(TreeNode root) {
         List<List<TreeNode>> res = new ArrayList<>();
         helper(res, root, 1);
         for(int i = 0; i < res.size(); i++) {
@@ -36,5 +36,39 @@ class Solution {
         if(root == null) return;
         helper(res, root.left, level + 1);
         helper(res, root.right, level + 1);
+    }
+    public boolean isSymmetricUsingStackIteratively(TreeNode root) {
+        List<List<TreeNode>> res = new ArrayList<>();
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s2.push(root);
+        int level = 1;
+        while(!s1.isEmpty() || !s2.isEmpty()) {
+            Stack<TreeNode> popS = level % 2 == 0 ? s1 : s2;
+            Stack<TreeNode> pushS = level % 2 == 0 ? s2 : s1;
+            TreeNode current = popS.pop();
+            if(res.size() < level) res.add(new ArrayList(Arrays.asList(current)));
+            else res.get(level - 1).add(current);
+            if(current != null) {
+                pushS.push(current.left);
+                pushS.push(current.right);
+            }
+            if(popS.isEmpty()) level++;
+        }
+        for(int i = 0; i < res.size(); i++) {
+            int p1 = 0;
+            int p2 = res.get(i).size() - 1;
+            while(p1 != p2) {
+                if(res.get(i).get(p1) == null || res.get(i).get(p2) == null) {
+                    if(res.get(i).get(p1) != null && res.get(i).get(p2) == null) return false;
+                    if(res.get(i).get(p1) == null && res.get(i).get(p2) != null) return false;
+                } else if(res.get(i).get(p1).val != res.get(i).get(p2).val) return false;
+                if(p1 + 1 == p2) break; // even case
+                // odd case
+                p1++;
+                p2--;
+            }
+        }
+        return true;
     }
 }
