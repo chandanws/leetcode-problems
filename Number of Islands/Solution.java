5136b7db96dc08582b86437c487913e8e87ae8bf
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-04-19 13:30:59 -0700

    Create Solution.java
    improved version of this would be instead pf using teh try and catch for out of boundary we actually check them

---
+++ Number of Islands/Solution.java
@@ -0,0 +1,31 @@
+// Problem link: https://leetcode.com/problems/number-of-islands/description/
+// Status: Accepted
+
+class Solution {
+    private void exploreTheIsland(int i, int j, char [][] grid) {
+        try {
+            if(grid[i][j] != '1') return;
+        } catch (Exception ex) {
+            return;
+        }
+        grid[i][j] = '2';
+        exploreTheIsland(i, j - 1, grid); // up
+        exploreTheIsland(i, j + 1, grid); // down
+        exploreTheIsland(i + 1, j, grid); // right
+        exploreTheIsland(i - 1, j, grid); // left
+    }
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        if(grid == null || rows == 0) return 0;
+        int cols = grid[0].length, numIslands = 0;
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(grid[i][j] == '1') {
+                    exploreTheIsland(i, j, grid);
+                    numIslands++;
+                }
+            }
+        }
+        return numIslands;
+    }
+}
