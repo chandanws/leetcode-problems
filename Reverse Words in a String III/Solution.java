67d5656a2f3039125bd1e27336835894ec51068e
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-04-25 19:14:30 -0700

    Update Solution.java

--- Reverse Words in a String III/Solution.java
+++ Reverse Words in a String III/Solution.java
@@ -2,6 +2,7 @@
 // Status: Accepted
 
 class Solution {
+    // slower when using append along StringBuilder
     private StringBuilder reverseString(String s) {
         int len = s.length();
         StringBuilder res = new StringBuilder();
@@ -9,6 +10,20 @@ class Solution {
             res.append(s.charAt(i));
         }
         return res;
+    }
+    // faster with swap with 2 pointers
+    private String reverseString(String s) {
+        int len = s.length(), i = 0, j = len - 1;
+        char [] charArray = s.toCharArray();
+        while(i != j) {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            if(i + 1 == j) break;
+            i++;
+            j--;
+        }
+        return String.valueOf(charArray);
     }
     public String reverseWords(String s) {
         String [] words = s.split(" ");
