a3b8d1a599fdb6fed58aa82a575a526ff28cb045
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-05-11 09:46:38 -0700

    Update Solution.java

--- Big Countries/Solution.java
+++ Big Countries/Solution.java
@@ -1,6 +1,6 @@
 // Problem link: https://leetcode.com/problems/big-countries/description/
 // Status: Accepted
 
-SELECT w.name, w.population, w.area
-FROM World w
-WHERE w.area > 3000000 OR w.population > 25000000;
+SELECT name, population, area
+FROM World
+WHERE area > 3000000 OR population > 25000000;
