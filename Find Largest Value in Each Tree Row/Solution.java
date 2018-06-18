3101109cb02a6e474623d3be2d51f28da3e56b65
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-06-18 12:52:57 -0700

    Update Solution.java

--- Find Largest Value in Each Tree Row/Solution.java
+++ Find Largest Value in Each Tree Row/Solution.java
@@ -11,7 +11,7 @@
  * }
  */
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
+    public List<Integer> largestValuesRecursiveWay(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();
         helper(root, res, 1);
         return res;
@@ -22,5 +22,22 @@ class Solution {
         if(res.get(level - 1) < root.val) res.set(level - 1, root.val);
         helper(root.left, res, level + 1);
         helper(root.right, res, level + 1);
+    }
+    public List<Integer> largestValuesInterativeWay(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size(), max = Integer.MIN_VALUE;
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if(node.val > max) max = node.val;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            res.add(max);
+        }
+        return res;
     }
 }
