a28626259408001deeb996579af59f9da7d5e503
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-06-21 21:09:24 -0700

    Update Solution.c

--- Missing Number/Solution.c
+++ Missing Number/Solution.c
@@ -1,3 +1,6 @@
+// Problem link: https://leetcode.com/problems/missing-number/description/
+// Status: Accepted
+
 int missingNumber(int *nums, int numsSize) {
     int remainBit = 0;
     for(int i = 0; i < numsSize; i++) {
