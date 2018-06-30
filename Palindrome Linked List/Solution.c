181656f9ffc312e39d8590a6520d7a2b5d637c7a
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-06-29 23:58:36 -0700

    Update Solution.c

--- Palindrome Linked List/Solution.c
+++ Palindrome Linked List/Solution.c
@@ -19,3 +19,34 @@ bool helper(struct ListNode** left, struct ListNode* right) {
 bool isPalindrome(struct ListNode* head) {
     return helper(&head, head);
 }
+
+bool isPalindromeWithoutRecur(struct ListNode* head) {
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+    int size = 1;
+    while(fast != NULL) {
+        slow = slow->next;
+        fast = fast->next;
+        size++;
+        if(fast != NULL) {
+            fast = fast->next;
+            size++;
+        }
+    }
+    if(slow == NULL) return true;
+    fast = head;
+    struct ListNode* newHead = NULL;
+    while(fast != slow) {
+        struct ListNode* next = fast->next;
+        fast->next = newHead;
+        newHead = fast;
+        fast = next;
+    }
+    if(size % 2 == 0) newHead = newHead->next;
+    while(newHead != NULL && slow != NULL) {
+        if(newHead->val != slow->val) return false;
+        newHead = newHead->next;
+        slow = slow->next;
+    }
+    return slow == NULL && newHead == NULL;
+}
