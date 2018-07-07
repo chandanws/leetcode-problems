12d39abf38e619d989b77e018350732ecbad7a0f
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-07-07 00:25:20 -0700

    Update Solution.java

--- First Missing Positive/Solution.java
+++ First Missing Positive/Solution.java
@@ -2,32 +2,16 @@
 // Status: Accepted
 
 class Solution {
-    private int getSmallestNum(int [] nums) {
-        int smallestNum = 0, len = nums.length;
-        for(int i = 0; i < len; i++) {
-            if(nums[i] < smallestNum) smallestNum = nums[i];
-        }
-        return Math.abs(smallestNum) + 1; // always add one here because zero cannot be negative
-    }
-    private int evenTheNegNum(int [] nums) {
-        int smallestNum = getSmallestNum(nums), len = nums.length;
-        if(smallestNum == 0) return smallestNum;
-        for(int i = 0; i < len; i++) {
-            nums[i] += smallestNum;
-        }
-        return smallestNum;
-    }
     public int firstMissingPositive(int[] nums) {
-        int len = nums.length, diff = evenTheNegNum(nums); // for example, -5 becomes 1 with diff of 6
-        for(int i = 0; i < len; i++) {
-            int index = Math.abs(nums[i]) - diff - 1;
-            if(index >= 0 && index < len && nums[index] > 0) nums[index] *= -1;
+        for(int i = 0; i < nums.length; i++) {
+            while(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                int j = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
         }
-        int res = 0;
-        while(res < len) {
-            if(nums[res] < 0) res++;
-            else break;
-        }
-        return res + 1;
+        for(int i = 0; i < nums.length; i++) if(nums[i] != i + 1) return i + 1;
+        return nums.length + 1;
     }
 }
