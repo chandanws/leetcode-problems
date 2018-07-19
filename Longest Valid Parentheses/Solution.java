0ee855c0fb1307da47f13d021a43568bf14a4215
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-07-19 10:49:24 -0700

    Create Solution.java

---
+++ Longest Valid Parentheses/Solution.java
@@ -0,0 +1,24 @@
+// Problem link: https://leetcode.com/problems/longest-valid-parentheses/
+// Status: Accepted
+
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxLength = 0;
+        Stack<Integer> st = new Stack<Integer>();
+        st.push(-1);
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if(st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - st.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
