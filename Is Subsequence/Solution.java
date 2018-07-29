8acc6ee3e2876638ad068364d76627870bb4f9fb
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-07-28 23:39:55 -0700

    Update Solution.java

--- Is Subsequence/Solution.java
+++ Is Subsequence/Solution.java
@@ -12,4 +12,23 @@ class Solution {
         }
         return n == 0 ? true : false;
     }
+    public boolean isSubsequenceBillionsOfSList(String s, String t) {
+        HashMap<Character, List<Integer>> hm = new HashMap<Character, List<Integer>>();
+        for(int i = 0; i < t.length(); i++) {
+            if(!hm.containsKey(t.charAt(i))) hm.put(t.charAt(i), new ArrayList<Integer>());
+            hm.get(t.charAt(i)).add(i);
+        }
+        int start = -1;
+        for(char c : s.toCharArray()) {
+            if(!hm.containsKey(c)) return false;
+            if(start == -1) start = hm.get(c).get(0);
+            else {
+                int next = Collections.binarySearch(hm.get(c), start + 1);
+                if(next < 0) next = Math.abs(next) - 1;
+                if(next >= hm.get(c).size() || hm.get(c).get(next) < start) return false;
+                start = hm.get(c).get(next);
+            }
+        }
+        return true;
+    }
 }
