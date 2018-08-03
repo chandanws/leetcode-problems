b077dd716c2b5986e2d34d19985262488948067c
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-08-02 21:29:01 -0700

    Create Solution.java

---
+++ Range Addition/Solution.java
@@ -0,0 +1,18 @@
+// Problem link: https://leetcode.com/problems/range-addition/description/
+// Status: Accepted
+
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int [] res = new int[length];
+        for(int i = 0; i < updates.length; i++) {
+            res[updates[i][0]] += updates[i][2];
+            if(updates[i][1] + 1 < length) {
+               res[updates[i][1] + 1] -= updates[i][2];
+            }
+        }
+        for(int i = 1; i < length; i++) {
+            res[i] += res[i - 1];
+        }
+        return res;
+    }
+}
