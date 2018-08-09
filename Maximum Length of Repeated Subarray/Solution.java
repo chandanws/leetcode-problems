a920fa9de863235ce629d7c0a94e12ef38a04807
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-08-09 14:08:37 -0700

    Create Solution.java

---
+++ Maximum Length of Repeated Subarray/Solution.java
@@ -0,0 +1,18 @@
+// Problem link: https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+// Status: Accepted
+
+class Solution {
+    public int findLength(int[] A, int[] B) {
+        int max = 0;
+        int [][] dp = new int[A.length + 1][B.length + 1];
+        for(int i = A.length - 1; i >= 0; i--) {
+            for(int j = B.length - 1; j >= 0; j--) {
+                if(A[i] == B[j]) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max;
+    }
+}
