2c683cd191d723defe68f1f81cd328ac19c99f3b
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-08-09 01:03:20 -0700

    Update Solution.java

--- Single Number/Solution.java
+++ Single Number/Solution.java
@@ -3,8 +3,8 @@
 
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = nums[0], len = nums.length;
-        for(int i = 1; i < len; i++) result ^= nums[i];
-        return result;
+        int res = 0;
+        for(int num : nums) res ^= num;
+        return res;
     }
 }
