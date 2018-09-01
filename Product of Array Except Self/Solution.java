5170f5a715ea657b8d863a9a85ee34d51be1381b
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-08-31 22:07:38 -0700

    Update Solution.java

--- Product of Array Except Self/Solution.java
+++ Product of Array Except Self/Solution.java
@@ -3,32 +3,20 @@
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if(nums.length == 0) { return nums; }
-        if(nums.length == 1) { return new int[]{1}; }
-        int [] output = new int[nums.length];
-        int times = 1;
-        for(int i = 0; i < nums.length; i++) {
-          times *= nums[i];
-          output[i] = times;
+        int [] res = new int[nums.length];
+        if(nums.length == 1) return new int[]{1};
+        res[0] = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            res[i] = nums[i] * res[i - 1];
         }
-        times = 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            nums[i] *= nums[i + 1];
+        }
         for(int i = nums.length - 1; i >= 0; i--) {
-          times *= nums[i];
-          nums[i] = times;
+            if(i == nums.length - 1) res[i] = res[i - 1];
+            else if(i == 0) res[i] = nums[i + 1];
+            else res[i] = res[i - 1] * nums[i + 1];
         }
-        int prevValue = 1;
-        for(int i = 0; i < nums.length; i++) {
-          if(i == 0) {
-            prevValue = output[i];
-            output[i] = nums[i + 1];
-          } else if(i + 1 == nums.length) {
-            output[i] = prevValue;
-          } else {
-            int temp = output[i];
-            output[i] = prevValue * nums[i + 1];
-            prevValue = temp;
-          }
-        }
-        return output;
+        return res;
     }
 }
