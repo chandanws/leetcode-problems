a69ce11047d0ca7a3e97a4753d576e624e4028ff
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-04 13:57:16 -0700

    Update Solution.java

--- Find Peak Element/Solution.java
+++ Find Peak Element/Solution.java
@@ -2,23 +2,14 @@
 // Status: Accepted
 
 class Solution {
-    private int helper(int [] nums, int start, int mid, int end) {
-        if(nums[start] > nums[start + 1]) return start;
-        if(nums[end] > nums[end - 1]) return end;
-        if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) return mid;
-        if(nums[start] > nums[mid] && nums[mid] > nums[end]) {
-            return helper(nums, start, (int) (mid - start) / 2 + start, mid);
-        } else if(nums[start] <= nums[mid] && nums[mid] >= nums[end]) {
-            if(nums[mid - 1] < nums[mid] && nums[mid] < nums[mid + 1]) {
-                return helper(nums, mid, (int) (end - mid) / 2 + mid, end);
-            }
-            return helper(nums, start, (int) (mid - start) / 2 + start, mid);
-        }
-        return helper(nums, mid, (int) (end - mid) / 2 + mid, end);
-    }
     public int findPeakElement(int[] nums) {
-        int len = nums.length;
-        if(len == 1) return 0;
-        return helper(nums, 0, (int) len / 2, len - 1);
+        int low = 0, high = nums.length - 1;
+        while(low < high) {
+            if(low == high) return low;
+            int mid = low + ((high - low) / 2);
+            if(nums[mid] < nums[mid + 1]) low = mid + 1;
+            else high = mid;
+        }
+        return low;
     }
 }
