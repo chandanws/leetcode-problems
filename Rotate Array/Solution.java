2b79f7f68ce017dcb00d25c360672a7dc3db3826
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-03 21:32:07 -0700

    Update Solution.java

--- Rotate Array/Solution.java
+++ Rotate Array/Solution.java
@@ -2,22 +2,20 @@
 // Status: Accepted
 
 class Solution {
-    public int [] reverseArr(int [] nums, int i, int j) {
-        int temp = 0;
+    private void reverse(int [] nums, int from, int to) {
+        int i = from, j = to - 1;
         while(i < j) {
-            temp = nums[i];
+            int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
             i++;
             j--;
         }
-        return nums;
     }
     public void rotate(int[] nums, int k) {
-        if(nums == null || nums.length < 2) { return; }
         k = k % nums.length;
-        nums = reverseArr(nums, nums.length - k, nums.length - 1);
-        nums = reverseArr(nums, 0, nums.length - k - 1);
-        nums = reverseArr(nums, 0, nums.length - 1);
+        reverse(nums, 0, nums.length);
+        reverse(nums, 0, k);
+        reverse(nums, k, nums.length);
     }
 }
