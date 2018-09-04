4309666c1999fa4f9d13ebdcbbb91cdb12193de4
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-04 13:58:22 -0700

    Update Solution.java

--- Find Peak Element/Solution.java
+++ Find Peak Element/Solution.java
@@ -4,8 +4,8 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int low = 0, high = nums.length - 1;
+        if(high == 0) return 0;
         while(low < high) {
-            if(low == high) return low;
             int mid = low + ((high - low) / 2);
             if(nums[mid] < nums[mid + 1]) low = mid + 1;
             else high = mid;
