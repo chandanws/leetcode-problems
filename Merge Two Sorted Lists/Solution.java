0be0bf7cf7badb7b124dcfb029fc9aa9aafee72b
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-05 18:56:46 -0700

    Update Solution.java

--- Merge Two Sorted Lists/Solution.java
+++ Merge Two Sorted Lists/Solution.java
@@ -9,40 +9,22 @@
  *     ListNode(int x) { val = x; }
  * }
  */
- 
- class Solution {
+class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null || l2 == null)
-          return l1 == null && l2 == null ? null : l1 == null ? l2 : l1;
-        ListNode head = l1.val < l2.val ? l1 : l2;
-        ListNode currNode = head, h1 = currNode == l1 ? l1.next : l1, h2 = currNode == l2 ? l2.next : l2;
-        while(h1 != null && h2 != null) {
-          if(h1.val == h2.val) {
-            currNode = currNode.next;
-            if(currNode == h1) {
-              h1 = h1.next;
-              currNode.next = h2;
-              h2 = h2.next;
-            } else if(currNode == h2) {
-              h2 = h2.next;
-              currNode.next = h1;
-              h1 = h1.next;
+        ListNode dummyHead = new ListNode(0);
+        ListNode currentNode = dummyHead;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                currentNode.next = l1;
+                l1 = l1.next;
+            } else {
+                currentNode.next = l2;
+                l2 = l2.next;
             }
-          } else if(h1.val > h2.val) {
-            currNode.next = h2;
-            h2 = h2.next;
-          } else if(h1.val < h2. val) {
-            currNode.next = h1;
-            h1 = h1.next;
-          }
-          currNode = currNode.next;
+            currentNode = currentNode.next;
         }
-        if(h1 == null && h2 != null) {
-          currNode.next = h2;
-        }
-        if(h2 == null && h1 != null) {
-          currNode.next = h1;
-        }
-        return head;
+        if(l1 != null) currentNode.next = l1;
+        else if(l2 != null) currentNode.next = l2;
+        return dummyHead.next;
     }
 }
