c7ec553c3b0a894dc437537fda4b890cb7ce7ae4
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-06 21:04:53 -0700

    Update Solution.java

--- Symmetric Tree/Solution.java
+++ Symmetric Tree/Solution.java
@@ -11,21 +11,21 @@
  * }
  */
 class Solution {
-    private void helper(TreeNode root, int level, List<List<Integer>> res) {
+    private void helper1(TreeNode root, int level, List<List<Integer>> res) {
         if(res.size() < level) res.add(new ArrayList<>());
         if(root != null) res.get(level - 1).add(root.val);
         else {
             res.get(level - 1).add(null);
             return;
         }
-        helper(root.left, level + 1, res);
-        helper(root.right, level + 1, res);
+        helper1(root.left, level + 1, res);
+        helper1(root.right, level + 1, res);
     }
     public boolean isSymmetricRecur(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if(root == null) return true;
-        helper(root.left, 1, res);
-        helper(root.right, 1, res);
+        helper1(root.left, 1, res);
+        helper1(root.right, 1, res);
         for(int i = 0; i < res.size(); i++) {
             if(res.get(i).size() % 2 == 1) return false;
             int start = 0, end = res.get(i).size() - 1;
@@ -35,36 +35,35 @@ class Solution {
         }
         return true;
     }
-    public boolean isSymmetricUsingStackIteratively(TreeNode root) {
-        List<List<TreeNode>> res = new ArrayList<>();
-        Stack<TreeNode> s1 = new Stack<>();
-        Stack<TreeNode> s2 = new Stack<>();
-        s2.push(root);
+    private void helper2(TreeNode root, List<List<Integer>> res) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
         int level = 1;
-        while(!s1.isEmpty() || !s2.isEmpty()) {
-            Stack<TreeNode> popS = level % 2 == 0 ? s1 : s2;
-            Stack<TreeNode> pushS = level % 2 == 0 ? s2 : s1;
-            TreeNode current = popS.pop();
-            if(res.size() < level) res.add(new ArrayList(Arrays.asList(current)));
-            else res.get(level - 1).add(current);
-            if(current != null) {
-                pushS.push(current.left);
-                pushS.push(current.right);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            if(res.size() < level) res.add(new ArrayList<>());
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if(node != null) {
+                    res.get(level - 1).add(node.val);
+                    q.offer(node.left);
+                    q.offer(node.right);
+                }
+                else res.get(level - 1).add(null);
             }
-            if(popS.isEmpty()) level++;
+            level++;
         }
+    }
+    public boolean isSymmetricUsingStackIteratively(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return true;
+        helper2(root.left, res);
+        helper2(root.right, res);
         for(int i = 0; i < res.size(); i++) {
-            int p1 = 0;
-            int p2 = res.get(i).size() - 1;
-            while(p1 != p2) {
-                if(res.get(i).get(p1) == null || res.get(i).get(p2) == null) {
-                    if(res.get(i).get(p1) != null && res.get(i).get(p2) == null) return false;
-                    if(res.get(i).get(p1) == null && res.get(i).get(p2) != null) return false;
-                } else if(res.get(i).get(p1).val != res.get(i).get(p2).val) return false;
-                if(p1 + 1 == p2) break; // even case
-                // odd case
-                p1++;
-                p2--;
+            if(res.get(i).size() % 2 == 1) return false;
+            int start = 0, end = res.get(i).size() - 1;
+            while(start < end) {
+                if(res.get(i).get(start++) != res.get(i).get(end--)) return false;
             }
         }
         return true;
