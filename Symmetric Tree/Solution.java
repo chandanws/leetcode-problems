251c107242d30c24b529e0189285ff2ca4f990e1
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-06 20:50:10 -0700

    Update Solution.java

--- Symmetric Tree/Solution.java
+++ Symmetric Tree/Solution.java
@@ -11,31 +11,29 @@
  * }
  */
 class Solution {
+    private void helper(TreeNode root, int level, List<List<Integer>> res) {
+        if(res.size() < level) res.add(new ArrayList<>());
+        if(root != null) res.get(level - 1).add(root.val);
+        else {
+            res.get(level - 1).add(null);
+            return;
+        }
+        helper(root.left, level + 1, res);
+        helper(root.right, level + 1, res);
+    }
     public boolean isSymmetricRecur(TreeNode root) {
-        List<List<TreeNode>> res = new ArrayList<>();
-        helper(res, root, 1);
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return true;
+        helper(root.left, 1, res);
+        helper(root.right, 1, res);
         for(int i = 0; i < res.size(); i++) {
-            int p1 = 0;
-            int p2 = res.get(i).size() - 1;
-            while(p1 != p2) {
-                if(res.get(i).get(p1) == null || res.get(i).get(p2) == null) {
-                    if(res.get(i).get(p1) != null && res.get(i).get(p2) == null) return false;
-                    if(res.get(i).get(p1) == null && res.get(i).get(p2) != null) return false;
-                } else if(res.get(i).get(p1).val != res.get(i).get(p2).val) return false;
-                if(p1 + 1 == p2) break; // even case
-                // odd case
-                p1++;
-                p2--;
+            if(res.get(i).size() % 2 == 1) return false;
+            int start = 0, end = res.get(i).size() - 1;
+            while(start < end) {
+                if(res.get(i).get(start++) != res.get(i).get(end--)) return false;
             }
         }
         return true;
-    }
-    private void helper(List<List<TreeNode>> res, TreeNode root, int level) {
-        if(res.size() < level) res.add(new ArrayList(Arrays.asList(root)));
-        else res.get(level - 1).add(root);
-        if(root == null) return;
-        helper(res, root.left, level + 1);
-        helper(res, root.right, level + 1);
     }
     public boolean isSymmetricUsingStackIteratively(TreeNode root) {
         List<List<TreeNode>> res = new ArrayList<>();
