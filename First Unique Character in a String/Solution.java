7798f34835d073d98762058f94944d6153b18ce6
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-07 17:06:46 -0700

    Update Solution.java

--- First Unique Character in a String/Solution.java
+++ First Unique Character in a String/Solution.java
@@ -3,23 +3,12 @@
 
 class Solution {
     public int firstUniqChar(String s) {
-        char [] charas = s.toCharArray();
-        LinkedHashMap<Character, Integer> t = new LinkedHashMap<Character, Integer>();
-        int i = 0;
-        for(char c : charas) {
-          if(t.containsKey(c)) {
-            t.put(c, -1);
-          } else {
-            t.put(c, i);
-          }
-          i++;
+        int [] alphabet = new int[26];
+        for(char c : s.toCharArray()) {
+            alphabet[c - 'a']++;
         }
-        Iterator itr = t.entrySet().iterator();
-        while(itr.hasNext()) {
-          Map.Entry me = (Map.Entry)itr.next();
-          if(Integer.valueOf((int)me.getValue()) != -1) {
-            return Integer.valueOf((int)me.getValue());
-          }
+        for(int i = 0; i < s.length(); i++) {
+            if(alphabet[s.charAt(i) - 'a'] == 1) return i;
         }
         return -1;
     }
