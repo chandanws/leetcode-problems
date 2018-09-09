a5aa08b4a0a37521346c864554e2463df9dc6d6d
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-09 14:50:42 -0700

    Update Solution.java

--- Reverse Words in a String II/Solution.java
+++ Reverse Words in a String II/Solution.java
@@ -2,26 +2,23 @@
 // Status: Accepted
 
 class Solution {
-    private void reverseString(char [] str, int i, int j) {
-        while(i != j) {
+    private void reverseWord(char[] str, int i, int j) {
+        while(i < j) {
             char c = str[i];
             str[i] = str[j];
             str[j] = c;
-            if(i + 1 == j) break;
             i++;
             j--;
         }
     }
-    public void reverseWords(char [] str) {
-        int len = str.length;
-        if(str == null || len < 2) return;
-        reverseString(str, 0, len - 1);
+    public void reverseWords(char[] str) {
+        reverseWord(str, 0, str.length - 1);
         int i = 0, j = 0;
-        while(i < len - 1 || j < len -1) {
-            while(j + 1 < len && str[j + 1] != ' ') j++;
-            reverseString(str, i, j);
-            j += 2;
-            i = j;
+        while(j < str.length) {
+            j = i;
+            while(j < str.length && str[j] != ' ') j++;
+            reverseWord(str, i, j - 1);
+            i = j + 1;
         }
     }
 }
