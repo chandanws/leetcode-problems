82e63456113d7027390c4914214d0adf6a2e274f
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-08 23:02:52 -0700

    Update Solution.java

--- Compare Version Numbers/Solution.java
+++ Compare Version Numbers/Solution.java
@@ -5,13 +5,11 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String [] v1 = version1.split("\\.");
         String [] v2 = version2.split("\\.");
-        int i1 = 0, i2 = 0;
-        while(i1 < v1.length || i2 < v2.length) {
-            int currentV1 = 0, currentV2 = 0;
-            if(i1 < v1.length) currentV1 = (int) Integer.parseInt(v1[i1++]);
-            if(i2 < v2.length) currentV2 = (int) Integer.parseInt(v2[i2++]);
-            if(currentV2 > currentV1) return -1;
-            else if(currentV1 > currentV2) return 1;
+        for(int i = 0; i < v1.length || i < v2.length; i++) {
+            int version1Num = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int version2Num = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if(version1Num > version2Num) return 1;
+            else if(version1Num < version2Num) return -1;
         }
         return 0;
     }
