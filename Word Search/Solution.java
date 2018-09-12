e1d5f0b69c791da84cac068eb08de581aa77374a
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-12 13:12:12 -0700

    Update Solution.java

--- Word Search/Solution.java
+++ Word Search/Solution.java
@@ -2,27 +2,23 @@
 // Status: Accepted
 
 class Solution {
-    private boolean dfs(char[][] board, String word, int i, int j) {
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || 
-           board[i][j] != word.charAt(0) ||
-           board[i][j] == '@') return false;
-        if(word.length() == 1) return true;
-        char temp = board[i][j];
-        board[i][j] = '@';
-        if(dfs(board, word.substring(1), i + 1, j)) return true;
-        if(dfs(board, word.substring(1), i - 1, j)) return true;
-        if(dfs(board, word.substring(1), i, j + 1)) return true;
-        if(dfs(board, word.substring(1), i, j - 1)) return true;
-        board[i][j] = temp;
+    private boolean dfs(char [][] board, char [] wordArr, int i, int j, int index) {
+        if(wordArr.length == index) return true;
+        if(i < 0 || j < 0 || i == board.length || j == board[0].length || wordArr[index] != board[i][j]) return false;
+        char c = board[i][j];
+        board[i][j] = '#';
+        if(dfs(board, wordArr, i + 1, j, index + 1)) return true;
+        if(dfs(board, wordArr, i - 1, j, index + 1)) return true;
+        if(dfs(board, wordArr, i, j + 1, index + 1)) return true;
+        if(dfs(board, wordArr, i, j - 1, index + 1)) return true;
+        board[i][j] = c;
         return false;
     }
     public boolean exist(char[][] board, String word) {
-        if(word.length() == 0 || board.length * board[0].length < word.length()) return false;
+        char [] wordArr = word.toCharArray();
         for(int i = 0; i < board.length; i++) {
-            for(int j =  0; j < board[0].length; j++) {
-                if(board[i][j] == word.charAt(0)) {
-                    if(dfs(board, word, i, j)) return true;
-                }
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, wordArr, i, j, 0)) return true;
             }
         }
         return false;
