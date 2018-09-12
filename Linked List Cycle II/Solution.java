9e7423d62c50e68757d257857694c6e5b7d6f4ed
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-11 17:59:07 -0700

    Update Solution.java

--- Linked List Cycle II/Solution.java
+++ Linked List Cycle II/Solution.java
@@ -13,42 +13,20 @@
  * }
  */
 public class Solution {
-    // Did not modify the Linked List but used extra spaces. O(N) for space complexity.
-    public static ListNode detectCycle(ListNode head) {
-        if(head == null) return null;
-        ListNode currentNode = head;
-        HashSet<ListNode> hs = new HashSet<ListNode>();
-        while(currentNode != null) {
-            if(hs.contains(currentNode)) return currentNode;
-            hs.add(currentNode);
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
-    // Did not modify the Linked List. Also, did not use extra space. Constant space complexity.
-    public static ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null) return null;
-        ListNode slow = head.next, fast = head.next.next, prevNode = null;
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
         while(fast != null) {
-            if(slow == fast) break;
-            prevNode = slow;
             slow = slow.next;
-            if(fast.next == null) fast = fast.next;
-            else fast = fast.next.next;
+            fast = fast.next;
+            if(fast != null) fast = fast.next;
+            if(slow == fast) break;
         }
         if(fast == null) return null;
-        ListNode firstNode = prevNode, secondNode = slow;
-        while(true) {
-            ListNode currentNode = head;
-            while(currentNode != firstNode && currentNode != secondNode) currentNode = currentNode.next;
-            if(currentNode == secondNode) break;
-            if(firstNode == prevNode && secondNode == slow) {
-                firstNode = currentNode == prevNode ? prevNode : slow;
-                secondNode = currentNode == prevNode ? slow : prevNode;
-            }
-            firstNode = firstNode.next;
-            secondNode = secondNode.next;
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
         }
-        return secondNode;
+        return slow;
     }
 }
