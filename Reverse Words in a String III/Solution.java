1ba2fbe20e74400b9ccbed38690737178161e325
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-14 16:49:34 -0700

    Update Solution.java

--- Reverse Words in a String III/Solution.java
+++ Reverse Words in a String III/Solution.java
@@ -2,37 +2,22 @@
 // Status: Accepted
 
 class Solution {
-    // slower when using append along StringBuilder
-    private StringBuilder reverseString(String s) {
-        int len = s.length();
-        StringBuilder res = new StringBuilder();
-        for(int i = len - 1; i >= 0; i--) {
-            res.append(s.charAt(i));
-        }
-        return res;
-    }
-    // faster with swap with 2 pointers
-    private String reverseString(String s) {
-        int len = s.length(), i = 0, j = len - 1;
-        char [] charArray = s.toCharArray();
-        while(i != j) {
-            char temp = charArray[i];
-            charArray[i] = charArray[j];
-            charArray[j] = temp;
-            if(i + 1 == j) break;
-            i++;
-            j--;
-        }
-        return String.valueOf(charArray);
-    }
     public String reverseWords(String s) {
-        String [] words = s.split(" ");
-        int len = words.length;
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < len; i++) {
-            res.append(reverseString(words[i]));
-            res.append(" ");
+        int i = 0, j = 0;
+        char [] sCharArr = s.toCharArray();
+        while(i < sCharArr.length) {
+            while(j < sCharArr.length && sCharArr[j] != ' ') j++;
+            int start = i, end = j - 1;
+            while(start < end) {
+                char c = sCharArr[start];
+                sCharArr[start] = sCharArr[end];
+                sCharArr[end] = c;
+                start++;
+                end--;
+            }
+            j++;
+            i = j;
         }
-        return res.toString().trim();
+        return new String(sCharArr);
     }
 }
