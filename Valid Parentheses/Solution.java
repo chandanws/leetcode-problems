5f73dd3fc264edc8aff992c81bc5e142eda9acc5
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-14 22:49:37 -0700

    Update Solution.java

--- Valid Parentheses/Solution.java
+++ Valid Parentheses/Solution.java
@@ -2,36 +2,14 @@
 // Status: Accepted
 
 class Solution {
-    final private char roundBracketOpen = '(';
-    final private char roundBracketClose = ')';
-    final private char curlyBracketOpen = '{';
-    final private char curlyBracketClose = '}';
-    final private char squareBracketOpen = '[';
-    final private char squareBracketClose = ']';
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
         for(char c : s.toCharArray()) {
-            switch(c) {
-                case roundBracketOpen:
-                    stack.push(roundBracketClose);
-                    break;
-                case roundBracketClose:
-                    if(stack.isEmpty() || !stack.isEmpty() && stack.pop() != roundBracketClose) return false;
-                    break;
-                case curlyBracketOpen:
-                    stack.push(curlyBracketClose);
-                    break;
-                case curlyBracketClose:
-                    if(stack.isEmpty() || !stack.isEmpty() && stack.pop() != curlyBracketClose) return false;
-                    break;
-                case squareBracketOpen:
-                    stack.push(squareBracketClose);
-                    break;
-                case squareBracketClose:
-                    if(stack.isEmpty() || !stack.isEmpty() && stack.pop() != squareBracketClose) return false;
-                    break;
-                default:
-                    break;
+            if(c == '(') stack.push(')');
+            else if(c == '{') stack.push('}');
+            else if(c == '[') stack.push(']');
+            else {
+                if(stack.isEmpty() || c != stack.pop()) return false;
             }
         }
         return stack.isEmpty();
