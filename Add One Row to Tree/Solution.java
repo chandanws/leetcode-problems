6d0854a6ac6e2e4c218337937a08e27744d52496
PoAn (Baron) Chen <chen.baron@hotmail.com>
2018-09-14 18:33:24 -0700

    Update Solution.java

--- Add One Row to Tree/Solution.java
+++ Add One Row to Tree/Solution.java
@@ -3,41 +3,35 @@
 
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-void helper(struct TreeNode* root, int v, int d, int level) {
-    if(root == NULL) return;
-    if(level + 1 == d) {
-        struct TreeNode *rootLeft = (struct TreeNode *) malloc(sizeof(struct TreeNode) * 1);
-        rootLeft->val = v;
-        rootLeft->left = root->left;
-        rootLeft->right = NULL;
-        struct TreeNode *rootRight = (struct TreeNode *) malloc(sizeof(struct TreeNode) * 1);
-        rootRight->val = v;
-        rootRight->left = NULL;
-        rootRight->right = root->right;
-        root->left = rootLeft;
-        root->right = rootRight;
-        return;
-    } else {
-        helper(root->left, v, d, level + 1);
-        helper(root->right, v, d, level + 1);
+class Solution {
+    private TreeNode helper(TreeNode root, int level, int v, int d) {
+        if(root == null) return null;
+        if(level + 1 == d) {
+            TreeNode leftSubtree = root.left;
+            TreeNode rightSubtree = root.right;
+            root.left = new TreeNode(v);
+            if(leftSubtree != null) root.left.left = leftSubtree;
+            root.right = new TreeNode(v);
+            if(rightSubtree != null) root.right.right = rightSubtree;
+        } else {
+            helper(root.left, level + 1, v, d);
+            helper(root.right, level + 1, v, d);
+        }
+        return root;
     }
-}
-
-struct TreeNode* addOneRow(struct TreeNode* root, int v, int d) {
-    if(root == NULL) return NULL;
-    else if(d == 1) {
-        struct TreeNode *newRoot = (struct TreeNode *) malloc(sizeof(struct TreeNode) * 1);
-        newRoot->val = v;
-        newRoot->left = root;
-        newRoot->right = NULL;
-        return newRoot;
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if(d == 1) {
+            TreeNode newRoot = new TreeNode(v);
+            newRoot.left = root;
+            return newRoot;
+        }
+        return helper(root, 1, v, d);
     }
-    helper(root, v, d, 1);
-    return root;
 }
