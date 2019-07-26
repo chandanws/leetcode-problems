0e8328c676dbd96838ac8a11fe942ca1cde39761
PoAn (Baron) Chen <chen.baron@hotmail.com>
2019-07-25 17:56:51 -0700

    Create Solution.java

---
+++ Corporate Flight Bookings/Solution.java
@@ -0,0 +1,16 @@
+// Problem link: https://leetcode.com/problems/corporate-flight-bookings/description/
+// Status: Accepted
+
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int [] answer = new int[n];
+        for(int [] booking : bookings) {
+            answer[booking[0] - 1] += booking[2];
+            if(booking[1] < n) answer[booking[1]] -= booking[2];
+        }
+        for(int i = 1; i < n; i++) {
+            answer[i] += answer[i - 1];
+        }
+        return answer;
+    }
+}
